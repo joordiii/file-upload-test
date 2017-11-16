c2a07cd3b1e09cc7abfa3b888ac76819c61da431
joordiii <joordiii@gmail.com>
2017-11-16 11:02:00 +0100

    added multer

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use stric';
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose=require('mongoose');
+
+const index = require('./routes/index');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
+mongoose.Promise=Promise;
+mongoose.connect('mongodb://localhost/lab-express-file-upload');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+const Picture = require('../models/pictures');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+//POST file with multier
+const upload = multer({
+  dest: './public/uploads/'
+});
+
+router
+  .post('/upload', upload.single('photo'), function (req, res, next) {
+
+    const pic = new Picture({
+      name: req.body.name,
+      pic_path: `/uploads/${req.file.filename}`, //This is multer stuff (boilerplate)
+      pic_name: req.file.originalname //This is multer stuff (boilerplate)
+    });
+
+    pic.save((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect('/');
+      }
+
+    });
+  });
+
 
 module.exports = router;
